951cb60dc5bdb07aba881a55c17c6a0234e492ea
LizzyKate <onyenekwelizabeth@gmail.com>
2023-10-04 14:36:05 +0100

    installed dependencies

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <component :is="$route.meta.layout || 'div'">
+    <router-view />
+  </component>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1 class="text-3xl text-red-900 font-bold underline">Hello world!</h1>
+</template>
